768160907c2dcfb69d4a6f3cd3a6b3d841b527d8
gaofeng <gaofeng@arcadedevhouse.com.au>
2023-04-11 10:01:15 +0800

    style update

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -250,10 +250,10 @@ export default () => {
             rows="1"
             class="gen-textarea"
           />
-          <button onClick={handleButtonClick} disabled={systemRoleEditing()} gen-slate-btn>
-            Send
+          <button onClick={handleButtonClick} disabled={systemRoleEditing()} gen-slate-btn style="width:80px;">
+            发送
           </button>
-          <button title="Clear" onClick={clear} disabled={systemRoleEditing()} gen-slate-btn>
+          <button title="清空会话" onClick={clear} disabled={systemRoleEditing()} gen-slate-btn>
             <IconClear />
           </button>
         </div>
